83af6d4844ea6a8aeb4ac16e4624f4b0f87b0607
Jose Flavio <jfnasc@outlook.com>
2016-09-30 10:33:44 -0300

    TL01 - Login

---
+++ sisfashion/web/src/main/java/br/com/sisfashion/apresentacao/login/UsuarioBean.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package br.com.sisfashion.apresentacao.login;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ * @author josen
+ * 
+ */
+@ManagedBean
+@SessionScoped
+public class UsuarioBean {
+
+    /**
+     * Realiza o logout do usuario invalidando sua sessão.
+     * 
+     * @return
+     */
+    public String logout() {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "/index.jsf?faces-redirect=true";
+    }
+}
