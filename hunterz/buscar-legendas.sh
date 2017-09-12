0240548f510ff6727a3d80fede73291d48400c6b
Jose Flavio Nascimento <jfnasc@outlook.com>
2017-09-12 11:31:54 -0300

    Buscar legendas

--- hunterz/buscar-legendas.sh
+++ hunterz/buscar-legendas.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+FOUND="0"
 SERIE="$1"
+
+SERIE="$(echo $SERIE | sed -e 's|ballers-2015|ballers|gi')"
 
 echo
 echo -----------------------------------------------------
@@ -7,60 +10,81 @@ echo $SERIE
 echo -----------------------------------------------------
 echo
 
-
 ##
 # So Legendas
 ##
 URL_ROOT="http://solegendas.com.br"
 echo "procurando em $URL_ROOT"
 
-url="$(wget -qO- "$URL_ROOT/$SERIE" | grep -io "$URL_ROOT/wp-content/uploads/[0-9a-zA-Z_/.]*.zip" | uniq)"
-if [ -n "$url" ];then
-  echo "encontrado! :)"
-  echo "$url"
-  wget "$url"
-  echo -----------------------------------------------------
-  echo
-  exit
+if [ "$FOUND" -eq "0" ];then
+  url="$(wget -qO- "$URL_ROOT/$SERIE" | grep -io "$URL_ROOT/wp-content/uploads/[0-9a-zA-Z_/.]*.zip" | uniq)"
+  if [ -n "$url" ];then
+    echo "encontrado! :)"
+    echo "$url"
+    wget "$url"
+    echo -----------------------------------------------------
+    echo
+    FOUND="1"
+  fi
 fi
 
-url="$(wget -qO- "$URL_ROOT/$SERIE" | grep -io "$URL_ROOT/?wpdmact=process&did=[0-9a-zA-Z=_]\+" | uniq)"
-if [ -n "$url" ];then
-  echo "encontrado! :)"
-  echo "$url"
-  wget "$url" -O $SERIE.rar
-  echo -----------------------------------------------------
-  echo
-  exit
+if [ "$FOUND" -eq "0" ];then
+  url="$(wget -qO- "$URL_ROOT/$SERIE" | grep -io "$URL_ROOT/?wpdmact=process&did=[0-9a-zA-Z=_]\+" | uniq)"
+  if [ -n "$url" ];then
+    echo "encontrado! :)"
+    echo "$url"
+    wget "$url" -O $SERIE.rar
+    echo -----------------------------------------------------
+    echo
+    FOUND="1"
+  fi
 fi
 
 ##
-# legendeu
+# Legendei
 ##
 URL_ROOT="http://legendei.com"
+
 echo "procurando em $URL_ROOT"
 
-url="$(wget -qO- "$URL_ROOT/$SERIE" | grep -io "?ofdl=[0-9]*" | uniq)"
-if [ -n "$url" ];then
-  echo "encontrado! :)"
-  echo "$url"
-  wget -O "$SERIE".rar "$URL_ROOT/$url"
-  echo -----------------------------------------------------
-  echo
-  exit
+if [ "$FOUND" -eq "0" ];then
+  url="$(wget -qO- "$URL_ROOT/$SERIE" | grep -io "?ofdl=[0-9]*" | uniq)"
+  if [ -n "$url" ];then
+    echo "encontrado! :)"
+    echo "$url"
+    wget -O "/tmp/$SERIE" "$URL_ROOT/$url"
+    echo -----------------------------------------------------
+    echo
+    FOUND="1"
+  fi
 fi
 
-url="$(wget -qO- "$URL_ROOT/$SERIE" | grep -io "$URL_ROOT/download/[0-9]*/" | uniq)"
-if [ -n "$url" ];then
-  echo "encontrado! :)"
-  echo "$url"
-  wget -O "$SERIE".rar "$url"
-  echo -----------------------------------------------------
-  echo
-  exit
+if [ "$FOUND" -eq "0" ];then
+  url="$(wget -qO- "$URL_ROOT/$SERIE" | grep -io "$URL_ROOT/download/[0-9]*/" | uniq)"
+  if [ -n "$url" ];then
+    echo "encontrado! :)"
+    echo "$url"
+    wget -O "/tmp/$SERIE" "$url"
+    echo -----------------------------------------------------
+    echo
+    FOUND="1"
+  fi
 fi
 
+if [ "$FOUND" -eq "1" ];then
+  echo "Encontrado! :)"
+  echo "Verificando o tipo de arquivo [$(file -bi "/tmp/$SERIE")]"
+  if [ -n "$(file -bi "/tmp/$SERIE" | grep zip)" ];then
+    echo "Movendo o arquivo..."
+    mv "/tmp/$SERIE" "./$SERIE.zip"  
+  fi
+  if [ -n "$(file -bi "/tmp/$SERIE" | grep rar)" ];then
+    echo "Movendo o arquivo..."
+    mv "/tmp/$SERIE" "./$SERIE.rar"  
+  fi
 
+  exit;
+fi
 
 echo "Nao encontrei! :("
 
